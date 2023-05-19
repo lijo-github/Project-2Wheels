857bb076a6fa97ffca291451e1c65f77d33fc2f5
lijo-github <lijogeorge389@gmail.com>
2023-05-19 11:54:42 +0530

    bin removed and app.js is modified

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const http = require("http");
+const debug = require("debug");
 const indexRouter = require("./routes/user");
 const adminRouter = require("./routes/admin");
-
 const app = express();
 const session = require("express-session");
 const { v4: uuid } = require("uuid");
@@ -28,8 +28,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
     session({
         secret: uuid(),
-        resave: true,
+        resave: false,
         saveUninitialized: false,
+        cookie: { maxAge: 600000 },
     })
 );
 
@@ -37,10 +38,7 @@ app.use("/", indexRouter);
 app.use("/admin", adminRouter);
 
 //*Database connection */
-connectDB()
-mongoose.connection.once("open", ()=>{
-    console.log("Database connected successfully");
-});
+connectDB();
 
 //* catch 404 and forward to error handler */
 app.use(function (req, res, next) {
@@ -57,5 +55,74 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error");
 });
+
+// Create HTTP server
+const server = http.createServer(app);
+
+// Set port
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+// Listen on provided port, on all network interfaces
+mongoose.connection.once("open", () => {
+    console.log("Database connected successfully");
+
+    server.listen(port, () => {
+        console.log(`Server running on port http://localhost:${port}`);
+    });
+});
+
+// Event listener for HTTP server "error" event
+const onError = (error) => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    const bind = typeof PORT === "string" ? `Pipe ${PORT}` : `Port ${PORT}`;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+server.on("error", onError);
+
+// Event listener for HTTP server "listening" event
+const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+    debug(`Listening on ${bind}`);
+};
+server.on("listening", onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
 
 module.exports = app;
